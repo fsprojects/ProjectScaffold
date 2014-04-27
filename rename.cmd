1480d72c3c338456f73467b46f6d8fc0cf41dfc3
gentauro <gentauro@gmail.com>
2014-04-27 17:05:26 +0200

    Removed dependency to Windows Environment PATH. Now the script will look for fsiAnyCPU at SDK 3.1 or 3.0

--- rename.cmd
+++ rename.cmd
@@ -1,3 +1,11 @@
 @echo off
-fsi.exe rename.fsx %*
+:: Add the paths for the F# SDK 3.x (from higher version to lower)
+set FSHARPSDK=^
+C:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0\;^
+C:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\
+
+cls
+:: Execute the script "only" with the first "fsianycpu.exe" found
+for %%i in (fsianycpu.exe) do "%%~$FSHARPSDK:i" rename.fsx %*
+
 pause
