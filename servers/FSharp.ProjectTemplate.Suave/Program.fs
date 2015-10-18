18a490282018d10635a3e79a8c16f89b6d633d82
davidpodhola <david.podhola@naseukoly.cz>
2015-10-18 12:24:52 +0200

    Suave server started

---
+++ servers/FSharp.ProjectTemplate.Suave/Program.fs
@@ -0,0 +1,5 @@
+﻿open Suave                 // always open suave
+open Suave.Http.Successful // for OK-result
+open Suave.Web             // for config
+
+startWebServer defaultConfig (OK "Hello World!")
