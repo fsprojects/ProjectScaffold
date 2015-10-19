766ee161f0572f6a7c54a07e8d5aa3f31a627c93
davidpodhola <david.podhola@naseukoly.cz>
2015-10-19 19:00:53 +0200

    Library used in ConsoleApplication

--- clients/FSharp.ProjectTemplate.ConsoleApplication/Program.fs
+++ clients/FSharp.ProjectTemplate.ConsoleApplication/Program.fs
@@ -1,7 +1,7 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open FSharp.ProjectTemplate
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    printfn "%A" (Library.hello 1)
     0 // return an integer exit code
