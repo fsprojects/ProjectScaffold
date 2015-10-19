7c58e9e6a7720cf5c7ce4d2132f0d1eddb922053
davidpodhola <david.podhola@naseukoly.cz>
2015-10-19 19:25:33 +0200

    upgrading MUIApp to https://github.com/fsprojects/FsXaml/tree/master/demos/WpfSimpleMvvmApplication

--- clients/FSharp.ProjectTemplate.MUIApp/MainView.xaml.fs
+++ clients/FSharp.ProjectTemplate.MUIApp/MainView.xaml.fs
@@ -2,5 +2,22 @@
 
 open FsXaml
 
-type MainView = XAML<"MainView.xaml">
+type MainView = XAML<"MainView.xaml", true>
+
+// This is to demonstrate being able to add in "code behind"
+// Note, in this case, this only displays a message when double clicking on 
+// the full name text box
+// see https://github.com/fsprojects/FsXaml/blob/master/demos/WpfSimpleMvvmApplication/MainView.xaml.fs
+type MainViewController() =
+    inherit UserControlViewController<MainView>()
+
+    let showMessage _ =
+        System.Windows.MessageBox.Show "You double clicked on Full Name!"
+        |> ignore
+
+    override this.OnLoaded view =                                
+        // Subscribe to the double click event, but also unsubscribe when we unload
+        view.tbFullName.MouseDoubleClick.Subscribe showMessage
+        |> this.DisposeOnUnload
+        
 
