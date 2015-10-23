d81ab1cb8e0a6f85490c12dcf36d1818feeeff70
davidpodhola <david.podhola@naseukoly.cz>
2015-10-24 00:18:40 +0200

    Orleans interface and grain projects added

---
+++ src/FSharp.ProjectTemplate.Grains/Library1.fs
@@ -0,0 +1,15 @@
+﻿namespace FSharp.ProjectTemplate.Grains
+
+open FSharp.ProjectTemplate
+open FSharp.ProjectTemplate.Interfaces
+open System
+open System.Threading.Tasks
+open Orleans
+
+type Grain1() = 
+    inherit Orleans.Grain()
+
+    interface IHello with
+
+        override this.SayHello(greeting:string) =
+            Task.FromResult<string>("This comes from F#!")
---
+++ src/FSharp.ProjectTemplate.Interfaces/IHello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSharp.ProjectTemplate.Interfaces
+{
+    public interface IHello : Orleans.IGrainWithIntegerKey
+    {
+        Task<string> SayHello(string greeting);
+    }
+}
