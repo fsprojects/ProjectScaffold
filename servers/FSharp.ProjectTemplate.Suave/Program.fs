71a464c3332b660c2e39f1076b3e615071222c54
davidpodhola <david.podhola@naseukoly.cz>
2015-11-05 00:17:29 +0100

    WIP RestFul Suave

--- servers/FSharp.ProjectTemplate.Suave/Program.fs
+++ servers/FSharp.ProjectTemplate.Suave/Program.fs
@@ -1,5 +1,26 @@
-﻿open Suave                 // always open suave
-open Suave.Http.Successful // for OK-result
-open Suave.Web             // for config
+﻿module App =
+    open Suave                 // always open suave
+    open Suave.Http.Successful // for OK-result
+    open Suave.Web             // for config
+    open RestFul
+    open FSharp.ProjectTemplate.Domain
 
-startWebServer defaultConfig (OK "Hello World!")
+    let createGreeting person =
+        let greet : Person = person
+        "Ahoj"
+
+    [<EntryPoint>]
+    let main argv = 
+        let personWebPart = rest "greetings" {
+            GetAll = ignore
+            GetById = ignore
+            IsExists = ignore
+            Update = ignore
+            UpdateById = ignore
+            Delete = ignore
+            Create = createGreeting
+        }
+
+        let app = choose[personWebPart;albumWebPart]                    
+
+        startWebServer defaultConfig app
