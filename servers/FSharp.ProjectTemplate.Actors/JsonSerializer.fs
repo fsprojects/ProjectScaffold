87b6841b418d60b4ef6687ea238c67e10c09cb96
davidpodhola <david.podhola@naseukoly.cz>
2015-11-06 16:55:33 +0100

    Orleans JObjectSerialization

--- servers/FSharp.ProjectTemplate.Actors/JsonSerializer.fs
+++ servers/FSharp.ProjectTemplate.Actors/JsonSerializer.fs
@@ -8,14 +8,26 @@ open Orleans.Serialization
 
 [<RegisterSerializerAttribute>]
 type JObjectSerialization() =
+    static do
+        SerializationManager.Register(
+            typeof<JObject>, 
+            SerializationManager.DeepCopier(JObjectSerialization.DeepCopier), 
+            SerializationManager.Serializer(JObjectSerialization.Serializer), 
+            SerializationManager.Deserializer(JObjectSerialization.Deserializer)
+        )
     static member DeepCopier (original:obj) =
         original
-    static member Serializer(untypedInput:obj, stream:BinaryTokenStreamWriter, expected:Type) =
+    static member Serializer(untypedInput:Object) (stream:BinaryTokenStreamWriter) (expected:Type) =
         let input : JObject = downcast untypedInput
         let str = input.ToString()
         SerializationManager.Serialize(str, stream);
-    static member Deserializer(expected:Type, stream:BinaryTokenStreamReader ) : obj =
+    static member Deserializer(expected:Type) (stream:BinaryTokenStreamReader) : obj =
         let str : string = downcast SerializationManager.Deserialize(typeof<string>, stream)
         upcast JObject.Parse(str)
     static member Register () =
-        SerializationManager.Register(typeof<JObject>)
+        SerializationManager.Register(
+            typeof<JObject>, 
+            SerializationManager.DeepCopier(JObjectSerialization.DeepCopier), 
+            SerializationManager.Serializer(JObjectSerialization.Serializer), 
+            SerializationManager.Deserializer(JObjectSerialization.Deserializer)
+        )
