a95543372c1e30e9b74b22958f10de0695e3b8ac
davidpodhola <david.podhola@naseukoly.cz>
2015-11-06 13:31:37 +0100

    WIP - not buildable

--- servers/FSharp.ProjectTemplate.Suave/Program.fs
+++ servers/FSharp.ProjectTemplate.Suave/Program.fs
@@ -7,19 +7,34 @@
     open Suave.Web
     open Suave.Types
     open RestFul
-
-    let createGreeting person =
-        let greet : Person = person
-        "Ahoj"
+    open System
+    open System.Reflection
+    open Orleankka.Playground
+    open FSharp.ProjectTemplate.Actors
+    open Orleankka.FSharp
 
     [<EntryPoint>]
     let main argv = 
+        use system = Orleankka.ActorSystem.Configure()
+                                .Playground()
+                                .Register(Assembly.GetExecutingAssembly())
+                                .Done()
+
+        let createGreeting (person: Person) =
+            let greeter = system.ActorOf(typeof<Greeter>, "")
+            let job() = task {
+                do! greeter <! Greet(person)
+            }
+            Task.run(job) |> ignore
+            "Ahoj"
+
         let app = 
           choose
             [ 
               POST >>= choose
                 [ path "/hello" >>= choose [ 
                     request (getResourceFromReq >> createGreeting >> JSON)  ] ] ]
+
 
         startWebServer defaultConfig app
         0
