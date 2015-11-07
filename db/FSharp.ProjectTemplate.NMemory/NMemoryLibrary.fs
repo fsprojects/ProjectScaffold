2b5faea1c0f0de8b01d0e43994917ae57fbe8616
davidpodhola <david.podhola@naseukoly.cz>
2015-11-07 23:45:01 +0100

    NMemory constructor done

--- db/FSharp.ProjectTemplate.NMemory/NMemoryLibrary.fs
+++ db/FSharp.ProjectTemplate.NMemory/NMemoryLibrary.fs
@@ -3,6 +3,10 @@
 open NMemory
 open NMemory.Tables
 open FSharp.ProjectTemplate.Domain
+open System.Linq.Expressions
+
+type Expr = 
+  static member Quote<'A, 'B>(e:Expression<System.Func<'A, 'B>>) = e
 
 type MyDatabase() = 
     inherit Database()
@@ -10,7 +14,7 @@ type MyDatabase() =
     let mutable persons : ITable<Person> = null
 
     do
-        persons <- base.Tables.Create<Person, string>(
+        persons <- base.Tables.Create<Person, string>( Expr.Quote<Person, string>( fun p -> p.FirstName+p.LastName ), IdentitySpecification<Person>(Expr.Quote<Person, int64>( fun p -> int64 (p.GetHashCode()) )) )
 
     member this.Persons 
         with get() = persons 
