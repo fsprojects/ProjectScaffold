4061108233ae83ff1b65ed622ec1b21e87b4e3ba
davidpodhola <david.podhola@naseukoly.cz>
2015-11-07 14:19:09 +0100

    WIP curry to allow persistency methods to be passed to the api implementation

--- src/FSharp.ProjectTemplate/Library.fs
+++ src/FSharp.ProjectTemplate/Library.fs
@@ -2,6 +2,7 @@
 
 open FSharp.ProjectTemplate.Domain
 open Serilog
+open System
 
 /// Documentation for my library
 ///
@@ -14,13 +15,20 @@ module Library =
   
     Log.Information( "Library FSharp.ProjectTemplate loaded" )
 
+    // the support methods
+    type SaveLastHello =
+        Person -> unit
+    type LoadLastHello =
+        Person -> DateTime
+
     /// Returns Hello firstName lastName, I saw you for the last time on 1.1.1970
     ///
     /// ## Parameters
     ///  - `person` - someone you would like to say hello to
-    let hello (person : Person) = 
-        sprintf "Hello %s %s" person.FirstName person.LastName
+    let hello (loadLastHello:LoadLastHello, saveLastHello:SaveLastHello) (person : Person) = 
+        sprintf "Hello %s %s, I saw you for the last time on %O" person.FirstName person.LastName (loadLastHello(person))
+        saveLastHello (person)
 
-    let api = {
-        Hello = hello 
+    let api (loadLastHello:LoadLastHello, saveLastHello:SaveLastHello) = {
+        Hello = hello (loadLastHello, saveLastHello)
     }
