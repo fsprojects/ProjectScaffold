3acdd2f06a50b26b9df6e529ec89b6ec7ccb8a83
davidpodhola <david.podhola@naseukoly.cz>
2015-11-08 22:43:38 +0100

    FSharp.ProjectTemplate.DbTests.SqlClient started

---
+++ tests/FSharp.ProjectTemplate.DbTests.SqlClient/SqlClientTests.fs
@@ -0,0 +1,22 @@
+﻿namespace FSharp.ProjectTemplate.DbTests
+
+module SqlClient =
+
+    open FSharp.ProjectTemplate
+    open NUnit.Framework
+    open FSharp.ProjectTemplate.Domain
+    open Serilog
+    open FSharp.ProjectTemplate.SqlClient
+    open System
+
+    Log.Logger <- LoggerConfiguration()
+        .Destructure.FSharpTypes()
+        .WriteTo.Console()
+        .CreateLogger()
+    Log.Information( "Tests started" )
+
+    [<Test>]
+    let ``simple SqlClient database crud is working`` () =
+      Assert.AreEqual( 1, 1 )
+
+      
