3fb2a940439392602687b4bac9149205d874679b
davidpodhola <david.podhola@naseukoly.cz>
2015-11-16 15:40:45 +0100

    WIP AutoFac

---
+++ support/Support.Autofac/Library1.fs
@@ -0,0 +1,19 @@
+﻿namespace Support
+
+module DI =
+    open Microsoft.Framework.Configuration
+    open Microsoft.Framework.Configuration.Json
+    open Autofac
+    open Autofac.Configuration
+
+    let doit<'T> () = 
+        // Add the configuration to the ConfigurationBuilder.
+        let config = new ConfigurationBuilder()
+        config.AddJsonFile("autofac.json") |> ignore
+
+        // Register the ConfigurationModule with Autofac.
+        let module1 = new ConfigurationModule(config.Build());
+        let builder = new ContainerBuilder();
+        builder.RegisterModule(module1)
+        let container = builder.Build()
+        container.Resolve<'T>()
