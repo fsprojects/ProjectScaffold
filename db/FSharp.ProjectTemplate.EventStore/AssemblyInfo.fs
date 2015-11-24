2ecc9dd38a2384bff9b6182c7199972fbca452e2
davidpodhola <david.podhola@naseukoly.cz>
2015-11-24 09:52:39 +0100

    FSharp.ProjectTemplate.EventStore started

---
+++ db/FSharp.ProjectTemplate.EventStore/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace FSharp.ProjectTemplate.EventStore.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FSharp.ProjectTemplate.EventStore")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FSharp.ProjectTemplate.EventStore")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("696da303-5795-491c-9fb6-701281ce416e")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ db/FSharp.ProjectTemplate.EventStore/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace FSharp.ProjectTemplate.EventStore
+
+type Class1() = 
+    member this.X = "F#"
