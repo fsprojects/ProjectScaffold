b32ea35397a4c80c550793f83fc92dea5e5b354f
Robin Munn <rmunn@pobox.com>
2016-06-05 19:38:30 +0700

    Handle FSIARGS correctly in zsh
    In zsh, a variable with a space in it will be treated as if it was
    quoted, which is not how bash behaves. There's a zsh-specific option you
    can set to consider it as two words instead, but that's not friendly to
    strictly-POSIX-compliant shells like dash. Instead, for maximum
    compatibility and simplicity, simply split it into two variables.

--- build.sh
+++ build.sh
@@ -9,10 +9,14 @@ PAKET_EXE=.paket/paket.exe
 FAKE_EXE=packages/build/FAKE/tools/FAKE.exe
 
 FSIARGS=""
+FSIARGS2=""
 OS=${OS:-"unknown"}
 if [ "$OS" != "Windows_NT" ]
 then
-  FSIARGS="--fsiargs -d:MONO"
+  # Can't use FSIARGS="--fsiargs -d:MONO" in zsh, so split it up
+  # (Can't use arrays since dash can't handle them)
+  FSIARGS="--fsiargs"
+  FSIARGS2="-d:MONO"
 fi
 
 run() {
@@ -69,5 +73,5 @@ run $PAKET_EXE restore
 
 [ ! -e build.fsx ] && run $PAKET_EXE update
 [ ! -e build.fsx ] && run $FAKE_EXE init.fsx
-run $FAKE_EXE "$@" $FSIARGS build.fsx
+run $FAKE_EXE "$@" $FSIARGS $FSIARGS2 build.fsx
 
