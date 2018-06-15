4a51f729a033af0306e908271008eee3aeaa694d
Jack Fox <jackfoxy@gmail.com>
2018-06-14 20:07:37 -0700

    support FAKE 5

--- build.cmd
+++ build.cmd
@@ -1,13 +1,9 @@
 @echo off
 cls
 
-.\paket.exe restore
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
+dotnet restore build.proj
 
 IF NOT EXIST build.fsx (
-  .\paket.exe update
-  packages\FAKE\tools\FAKE.exe init.fsx
+  dotnet fake init.fsx
 )
-packages\FAKE\tools\FAKE.exe build.fsx %*
+dotnet fake build %*
