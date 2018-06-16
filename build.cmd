5cc9ca6c0159801b1ecbeaff6331166bbecad0a5
Jack Fox <jackfoxy@gmail.com>
2018-06-16 10:02:33 -0700

    FAKE 5.0

--- build.cmd
+++ build.cmd
@@ -1,18 +1,9 @@
 @echo off
 cls
 
-.paket\paket.bootstrapper.exe
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-.paket\paket.exe restore
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
+dotnet restore build.proj
 
 IF NOT EXIST build.fsx (
-  .paket\paket.exe update
-  packages\FAKE\tools\FAKE.exe init.fsx
+  dotnet fake init.fsx
 )
-packages\FAKE\tools\FAKE.exe build.fsx %*
+dotnet fake build %*
